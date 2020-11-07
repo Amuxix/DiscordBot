5f5252ca206199c0e32914cd08fc529e46eecb93
Amuxix <amuxix@gmail.com>
2020-11-07 23:01:58 +0000

    Stop after a valid command is ran

--- src/main/scala/me/amuxix/Listener.scala
+++ src/main/scala/me/amuxix/Listener.scala
@@ -4,12 +4,13 @@ import cats.effect.concurrent.Ref
 import cats.effect.{IO, Timer}
 import cats.implicits._
 import fs2.Stream
+import net.dv8tion.jda.api.JDA
 import net.dv8tion.jda.api.entities._
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent
 import net.dv8tion.jda.api.events.message.priv.PrivateMessageReceivedEvent
 import net.dv8tion.jda.api.hooks.ListenerAdapter
-import net.dv8tion.jda.api.JDA
+import net.dv8tion.jda.api.requests.RestAction
 
 import scala.concurrent.duration._
 import scala.jdk.CollectionConverters._
@@ -17,11 +18,11 @@ import scala.util.Random
 import scala.util.Random.nextInt
 
 class Listener(implicit timer: Timer[IO]) extends ListenerAdapter {
-  val spamList: Ref[IO, Set[Long]] =  Ref.unsafe[IO, Set[Long]](Set.empty)
-  val userMap: Ref[IO, Map[Long, User]] =  Ref.unsafe[IO, Map[Long, User]](Map.empty)
-  val muteLeader: Ref[IO, Option[(Long, Boolean)]] =  Ref.unsafe[IO, Option[(Long, Boolean)]](Option.empty)
-  val replacements: Ref[IO, Map[String, String]] =  Ref.unsafe[IO, Map[String, String]](Map.empty)
-  val cersibon: Ref[IO, Boolean] =  Ref.unsafe[IO, Boolean](false)
+  val spamList: Ref[IO, Set[Long]] = Ref.unsafe[IO, Set[Long]](Set.empty)
+  val userMap: Ref[IO, Map[Long, User]] = Ref.unsafe[IO, Map[Long, User]](Map.empty)
+  val muteLeader: Ref[IO, Option[(Long, Boolean)]] = Ref.unsafe[IO, Option[(Long, Boolean)]](Option.empty)
+  val replacements: Ref[IO, Map[String, String]] = Ref.unsafe[IO, Map[String, String]](Map.empty)
+  val cersibon: Ref[IO, Boolean] = Ref.unsafe[IO, Boolean](false)
 
   val possibleResponses = List(
     "It is certain.",
@@ -46,14 +47,14 @@ class Listener(implicit timer: Timer[IO]) extends ListenerAdapter {
     "Very doubtful.",
   )
 
-  def aioque(channel: MessageChannel, author: User, message: Message) = {
+  def aioque(channel: MessageChannel, author: User, message: Message): IO[Boolean] = {
     if (author.getName.toLowerCase.contains("velo") && nextInt(100) == 0) {
       for {
-        _ <- IO(message.addReaction("U+1F923").queue())
-        _ <- IO(channel.sendMessage(possibleResponses(nextInt(possibleResponses.size))).queue())
-      } yield ()
+        _ <- message.addReaction("U+1F923").run
+        _ <- channel.sendMessage(possibleResponses(nextInt(possibleResponses.size))).run
+      } yield false
     } else {
-      IO.unit
+      IO.pure(false)
     }
   }
 
@@ -62,31 +63,31 @@ class Listener(implicit timer: Timer[IO]) extends ListenerAdapter {
       case (string, (regex, replacement)) => string.replaceAll(regex, replacement)
     }
 
-  def replace(message: Message, channel: MessageChannel): IO[Unit] = {
+  def replace(message: Message, channel: MessageChannel): IO[Boolean] = {
     val content = message.getContentRaw
     replacements.get.flatMap { replacements =>
       val replacedContent = applyReplacements(content, replacements)
       if (!replacedContent.equals(content)) {
-        IO(channel.sendMessage(s"*$replacedContent*").queue())
+        channel.sendMessage(s"*$replacedContent*").run.as(false)
       } else {
-        IO.unit
+        IO.pure(false)
       }
     }
   }
 
-  def addReplacement(message: Message): IO[Unit] = {
+  def addReplacement(message: Message): IO[Boolean] = {
     val replace = "^replace (.+),, (.+)$".r
     message.getContentRaw match {
-      case replace(regex, replacement) => replacements.update(_ + (regex -> replacement))
-      case _ => IO.unit
+      case replace(regex, replacement) => replacements.update(_ + (regex -> replacement)).as(true)
+      case _                           => IO.pure(false)
     }
   }
 
-  def removeReplacement(message: Message): IO[Unit] = {
+  def removeReplacement(message: Message): IO[Boolean] = {
     val replace = "^stop replacing (.+)$".r
     message.getContentRaw match {
-      case replace(regex) => replacements.update(_ - regex)
-      case _ => IO.unit
+      case replace(regex) => replacements.update(_ - regex).as(true)
+      case _              => IO.pure(false)
     }
   }
 
@@ -102,6 +103,7 @@ class Listener(implicit timer: Timer[IO]) extends ListenerAdapter {
       } else {
         word
       }
+
     words.split(" ").map(swapPair).mkString(" ")
   }
 
@@ -115,28 +117,30 @@ class Listener(implicit timer: Timer[IO]) extends ListenerAdapter {
     "Portugues" -> "p'rt'gêsh",
   )
 
-  def callCersibon(message: Message, channel: MessageChannel): IO[Unit] =
+  def callCersibon(message: Message, channel: MessageChannel): IO[Boolean] =
     message.command("cersibon") {
       cersibon.get.flatMap { is =>
         val message = if (!is)
-          IO(channel.sendMessage("A wild cersibon appears!").queue())
+          channel.sendMessage("A wild cersibon appears!").run
         else {
-          IO(channel.sendMessage("Cersibon goes away!").queue())
+          channel.sendMessage("Cersibon goes away!").run
         }
         message *> cersibon.set(!is)
       }
     }
 
-  def cersibonize(message: Message, channel: MessageChannel): IO[Unit] =
+  def cersibonize(message: Message, channel: MessageChannel): IO[Boolean] =
     cersibon.get.flatMap { is =>
       if (is) {
         val content = message.getContentRaw
         val random = new Random(content.hashCode)
+
         def nextInt(int: Int) = random.nextInt(int)
+
         val cersibonized = dropLetter(swapPairs(applyReplacements(content, shorthands), nextInt), nextInt)
-        IO(channel.sendMessage(cersibonized).queue())
+        channel.sendMessage(cersibonized).run.as(false)
       } else {
-        IO.unit
+        IO.pure(false)
       }
     }
 
@@ -150,7 +154,8 @@ class Listener(implicit timer: Timer[IO]) extends ListenerAdapter {
         user.openPrivateChannel().queue { channel =>
           channel.sendMessage("Spam").queue()
         }
-      ))
+      )
+      )
     } yield ()
 
   def followMute(jda: JDA): Stream[IO, Unit] =
@@ -167,19 +172,20 @@ class Listener(implicit timer: Timer[IO]) extends ListenerAdapter {
         } yield ()
       } else {
         IO.unit
-      })
+      },
+      )
     } yield ()
 
-  def addToSpam(message: Message): IO[Unit] = {
+  def addToSpam(message: Message): IO[Boolean] = {
     val spamRegex = "spam <@!(\\d+)>".r
     message.getContentRaw.toLowerCase match {
       case spamRegex(id) =>
         for {
           user <- getUser(id.toLong, message.getJDA)
           _ = println(s"Spamming ${user.getName}")
-          _ <- if (!user.isBot) spamList.update(_ + user.getIdLong) else IO.unit
-        } yield ()
-      case _ => IO.unit
+          stop <- if (!user.isBot) spamList.update(_ + user.getIdLong).as(true) else IO.pure(false)
+        } yield stop
+      case _             => IO.pure(false)
     }
   }
 
@@ -193,80 +199,86 @@ class Listener(implicit timer: Timer[IO]) extends ListenerAdapter {
       }(IO.pure)
     }
 
-  def removeFromSpam(user: User): IO[Unit] =
+  def removeFromSpam(user: User): IO[Boolean] =
     for {
-      _ <- IO(println(s"Stopped spamming ${user.getName}"))
-      _ <- spamList.update(_ - user.getIdLong)
-    } yield ()
+      list <- spamList.get
+      id = user.getIdLong
+      stop <- if (list.contains(id)) {
+        IO(println(s"Stopped spamming ${user.getName}")) *> spamList.update(_ - id).as(true)
+      } else {
+        IO.pure(false)
+      }
+    } yield stop
 
-  def findVoiceChannel(user: User): Option[VoiceChannel] =
-    user.getJDA.getVoiceChannels.asScala.toList.collectFirst {
-      case channel if channel.getMembers.asScala.exists(_.getUser.getIdLong == user.getIdLong) => channel
-    }
-
-  def muteAll(message: Message, author: User): IO[Unit] =
+  def muteAll(message: Message, author: User): IO[Boolean] =
     message.command("muteall") {
-      findVoiceChannel(author).traverse_ { channel =>
+      author.findVoiceChannel.traverse_ { channel =>
         channel.muteAll
       }
     }
 
-  def followMute(message: Message, author: User): IO[Unit] = {
+  def followMute(message: Message, author: User): IO[Boolean] =
     message.command("followmute") {
-      println("Updating leader")
       muteLeader.update {
         case Some((id, _)) if id == author.getIdLong => None
-        case None =>
-          println(s"No leader, adding ${author.getName} as leader.")
-          author.isSelfMuted.map(author.getIdLong -> _)
-        case some => some
+        case None                                    => author.isSelfMuted.map(author.getIdLong -> _)
+        case some                                    => some
       }
     }
-  }
 
   implicit class UserOps(user: User) {
     def findVoiceChannel: Option[VoiceChannel] = user.getJDA.getVoiceChannels.asScala.toList.collectFirst {
       case channel if channel.getMembers.asScala.exists(_.getUser.getIdLong == user.getIdLong) => channel
     }
 
-    def member: Option[Member] = findVoiceChannel.flatMap(_.getMembers.asScala.find(_.getUser.getIdLong == user.getIdLong))
+    def member: Option[Member] = findVoiceChannel.flatMap(_.getMembers
+      .asScala
+      .find(_.getUser.getIdLong == user.getIdLong),
+    )
 
     def isSelfMuted: Option[Boolean] = member.map(_.getVoiceState.isSelfMuted)
   }
+
   implicit class VoiceChannelOps(channel: VoiceChannel) {
     def muteAll: IO[Unit] =
       channel.getMembers.asScala.toList.traverse_ { member =>
         val isMuted = member.getVoiceState.isGuildMuted
-        IO(member.mute(!isMuted).queue())
+        member.mute(!isMuted).run
       }
   }
 
   implicit class MessageOps(message: Message) {
-    def command(commands: String*)(f: => IO[Unit]) = {
+    def command(commands: String*)(f: => IO[Unit]): IO[Boolean] = {
       val content = message.getContentRaw
-      if (commands.exists(content.equalsIgnoreCase)) f else IO.unit
+      if (commands.exists(content.equalsIgnoreCase)) f.as(true) else IO.pure(false)
     }
+  }
+
+  implicit class IOOps(io: IO[Boolean]) {
+    def orStop(stop: Boolean): IO[Boolean] = if (stop) IO.pure(stop) else io
+  }
+
+  implicit class ActionOps[T](action: RestAction[T]) {
+    def run: IO[Unit] = IO(action.queue())
   }
 
   override def onMessageReceived(event: MessageReceivedEvent): Unit = {
     if (!event.getAuthor.isBot) {
-      println(s"Got a message ${event.getAuthor.getName} - ${event.getMessage.getContentRaw}")
       (for {
-        _ <- aioque(event.getChannel, event.getAuthor, event.getMessage)
-        _ <- muteAll(event.getMessage, event.getAuthor)
-        _ <- followMute(event.getMessage, event.getAuthor)
-        _ <- replace(event.getMessage, event.getChannel)
-        _ <- addReplacement(event.getMessage)
-        _ <- removeReplacement(event.getMessage)
-        _ <- cersibonize(event.getMessage, event.getChannel)
-        _ <- callCersibon(event.getMessage, event.getChannel)
+        muteAll <- muteAll(event.getMessage, event.getAuthor)
+        followMute <- followMute(event.getMessage, event.getAuthor).orStop(muteAll)
+        addReplacement <- addReplacement(event.getMessage).orStop(followMute)
+        removeReplacement <- removeReplacement(event.getMessage).orStop(addReplacement)
+        callCersibon <- callCersibon(event.getMessage, event.getChannel).orStop(removeReplacement)
+        aioque <- aioque(event.getChannel, event.getAuthor, event.getMessage).orStop(callCersibon)
+        replace <- replace(event.getMessage, event.getChannel).orStop(aioque)
+        cersibonize <- cersibonize(event.getMessage, event.getChannel).orStop(replace)
       } yield ()).unsafeRunSync()
     }
   }
 
   override def onGuildMessageReceived(event: GuildMessageReceivedEvent): Unit = {
     if (!event.getAuthor.isBot) {
-      println(s"Got a message ${event.getAuthor.getName} - ${event.getMessage.getContentRaw}")
       addToSpam(event.getMessage).unsafeRunSync()
     }
   }
